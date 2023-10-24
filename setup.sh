1e434c55cc291004d8e08985b533379151c639f3
Alessandro Perazzetta <482310+AlessandroPerazzetta@users.noreply.github.com>
2023-10-24 12:36:19 +0200

    First commit

---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root or using sudo"
+   exit 1
+fi
+
+if ! [ -x "$(command -v figlet)" ]; then
+  apt install figlet
+fi
+if ! [ -x "$(command -v lsb_release)" ]; then
+  apt install lsb-release
+fi
+
+cp ./update-motd.d/* /etc/update-motd.d/
+
+mv /etc/motd /etc/motd.ORI
+mv /etc/issue /etc/issue.ORI
+
+
+cat <<'EOF' > /etc/network/if-up.d/update-issue
+#!/bin/sh
+#
+#    update-issue - write info to /etc/issue at boot
+#
+set -e
+
+HEADER=$(/etc/update-motd.d/00-header)
+SYSINFO=$(/etc/update-motd.d/10-sysinfo)
+
+echo "$HEADER\n\n$SYSINFO" > /etc/issue
+EOF
+
+chmod +x /etc/network/if-up.d/update-issue
